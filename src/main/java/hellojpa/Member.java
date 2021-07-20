8f444e1bf150f3fc30b1c8933ba3e23b3568998b
KingBlackCow <sgs1159@naver.com>
2021-07-20 23:10:15 +0900

    init

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,32 @@
+package hellojpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+//@Table(name = "User") //이러면 원래는 Member이지만 USER테이블에 저장
+
+public class Member {
+    @Id
+    private Long id;
+
+    //@Column(name = "username") username 칼럼에 저장장
+   private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
